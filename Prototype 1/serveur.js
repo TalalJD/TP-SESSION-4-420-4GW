3f8945b20d9155e3b9cae704d5a6e5088c7e071e
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2024-02-12 16:38:49 -0500

    setup de styles, public

--- Prototype 1/serveur.js
+++ Prototype 1/serveur.js
@@ -17,7 +17,19 @@ const __dirname = path.dirname(__filename);
 /*
 Connexion au serveur
 */
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 const server = app.listen(4000, function() {
 console.log("serveur fonctionne sur 4000... ! ");
 });
+app.use(express.static('public'));
+app.get("/", function (req, res) {
+    // No database query, just render the page
+    res.render("Pages/index", {
+      siteTitle: "Simple Application",
+      pageTitle: "Event List",
+      items: [] // Assuming 'items' is used in your EJS file, pass an empty array or appropriate default value
+    });
+  });

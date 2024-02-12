aabdb95077c9f91061d43c51159bb16bf1241f63
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-02-12 16:13:57 -0500

    Creation du serveur

---
+++ Prototype 1/serveur.js
@@ -0,0 +1,23 @@
+/*
+Importation des modules requis
+*/
+
+
+import express from "express";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from "url";
+import mysql from "mysql";
+import { body, validationResult } from "express-validator";
+import dateFormat from "dateformat";
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/*
+Connexion au serveur
+*/
+
+const server = app.listen(4000, function() {
+console.log("serveur fonctionne sur 4000... ! ");
+});

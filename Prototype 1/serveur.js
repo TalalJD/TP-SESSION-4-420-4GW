b2f74e32cf0cfa607f982e530cec34f687153c25
sultanTrash <115166309+sultanTrash@users.noreply.github.com>
2024-02-26 17:03:38 -0500

    Requete pour page indexSport

--- Prototype 1/serveur.js
+++ Prototype 1/serveur.js
@@ -90,6 +90,26 @@ app.post("/inscription/submit", function (req, res) {
     res.redirect("/");
   });
 });
-
+app.get("/indexSport", function(req,res){
+  const id_sport=req.query.id_sport;
+  const image=req.query.image;
+  const name=req.query.name;
+  const request =
+    "SELECT * FROM poste WHERE sport_id_sport=?";
+  con.query(request,[id_sport], function(err, result){
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error"); 
+      return;
+    }
+    res.render("Pages/indexSport.ejs", {
+      siteTitle: "Simple Application",
+      pageTitle: "Event List",
+      items: result,
+      image: image,
+      name: name
+    });
+  });
+});
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 

9207aa25683a8a2e977861c8ea4b09d954418785
Hamza Oumeziane <2226562@bdeb.qc.ca>
2024-03-07 10:11:18 -0500

    Régler le problème des pages vide en cas d'erreur
    J'ai fait les commentaires aussi

---
+++ Prototype 1/public/connexion.js
@@ -0,0 +1,37 @@
+// le script pour faire la connexion d'un compte energymize
+// Ce code est inspiré d'un code de Chatgpt
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Récupérer l'adresse email saisie 
+    let adresse_mail = document.getElementById('adresse_mail').value;
+    // Récupérer le mot de passe saisi
+    let mdp = document.getElementById('mdp').value;
+  
+    // Utilise l'API Fetch pour envoyer une requête POST au serveur avec l'adresse e-mail et le mot de passe de l'utilisateur.
+    fetch('/connexion/submit', {
+      method: 'POST',
+      headers: {
+        // Indique au serveur que les données envoyées sont au format JSON
+        'Content-Type': 'application/json', 
+      },
+      // Convertit les données du formulaire en chaîne JSON.
+      body: JSON.stringify({ adresse_mail: adresse_mail, mdp: mdp }) 
+    })
+    // Traite la réponse et la convertit en JSON.
+    .then(response => response.json()) 
+    .then(data => {
+      if (data.success) {
+        // Si la connexion est réussie, redirige l'utilisateur vers la page d'accueil.
+        window.location.href = '/'; 
+      } else {
+        let alerteErreur = document.getElementById('alerteErreur');
+        alerteErreur.style.display = 'block';
+        // Affiche le message d'erreur envoyé par le serveur.
+        alerteErreur.textContent = data.message; 
+      }
+    })
+    .catch((error) => {
+        // Affiche l'erreur dans la console si la requête échoue.
+      console.error('Error:', error); 
+    });
+  });

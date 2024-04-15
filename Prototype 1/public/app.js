e7080a58d0d2e79db2a747089aadefc60e11a80e
TalalJD <120325175+TalalJD@users.noreply.github.com>
2024-04-15 17:40:09 -0400

    PROFILE ON THE APP RAAAAAAAH

--- Prototype 1/public/app.js
+++ Prototype 1/public/app.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const plusIcon = document.querySelector('[name="plusIcon"]');
-    const cardContainer = document.querySelector('.card-container');
-
-    plusIcon.addEventListener('click', function() {
-        cardContainer.style.display = 'block'; // Affiche les cartes
-        cardContainer.style.transform = 'translateY(0)'; // Animation pour afficher les cartes
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("profileIcon").addEventListener("click", function() {
+        var profilePage = document.querySelector(".profile-page");
+        profilePage.classList.toggle("hidden");
+        profilePage.classList.toggle("show");
     });
 });

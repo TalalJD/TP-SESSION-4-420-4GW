f224d0f9ec083c46d833276b20d510c3135fa054
houssem342503 <2253158@bdeb.qc.ca>
2024-04-17 14:35:46 -0400

    Revert "Merge branch 'main' of https://github.com/TalalJD/TP-SESSION-4-420-4GW"
    This reverts commit 0e28d8350e595462dbdc4192e3ddedba3cc74cf4, reversing
    changes made to 337259bd15c3ef1e48bf1b2f0e6ab2f15f79a4fa.

--- Prototype 1/public/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("profileIcon").addEventListener("click", function() {
-        var profilePage = document.querySelector(".profile-page");
-        profilePage.classList.toggle("hidden");
-        profilePage.classList.toggle("show");
-        document.body.style.overflow = profilePage.classList.contains("show") ? "hidden" : "auto";
-
-    });
-});
